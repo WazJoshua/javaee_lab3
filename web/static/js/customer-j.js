6523f957165d3fafb772c4d6da806b0a9de8983f
bbimix <2505520629@qq.com>
2020-11-16 00:14:03 +0800

    添加了黑夜模式

--- web/static/js/customer-j.js
+++ web/static/js/customer-j.js
@@ -492,3 +492,16 @@ $("#selectDict").click(
         to_page2(1);
     }
 )
+/*************************************************获取cookie值**********************************************************/
+function getCookie(name){
+    var strcookie = document.cookie;//获取cookie字符串
+    var arrcookie = strcookie.split("; ");//分割
+    //遍历匹配
+    for ( var i = 0; i < arrcookie.length; i++) {
+        var arr = arrcookie[i].split("=");
+        if (arr[0] == name){
+            return arr[1];
+        }
+    }
+    return "";
+}
